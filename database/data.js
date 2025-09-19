0297ba3e01969eb4b9a293196a0c5be2d5b9a7f4
jinaybothra <jinaydeep2001@gmail.com>
2025-09-19 05:54:48 +0530

    added attributes to Schema

--- database/data.js
+++ database/data.js
@@ -5,10 +5,14 @@ dotenv.config();
 const MONGODBURL = process.env.MONGODBURL;
 const dbConnection = async ()=>{
     try{
-        await mongoose.connect(MONGODBURL);
+        await mongoose.connect(MONGODBURL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
     }
     catch{
         console.log("Error while connecting to Database");
+        process.exit(1);
     }
 }
 module.exports = {dbConnection};
--- models/User.js
+++ models/User.js
@@ -3,16 +3,23 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name:{
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     email:{
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true,
+        lowercase: true
     },
     password:{
         type: String,
-        required: true
+        required: true,
+        minlength:[6, 'Password must be 6 letters long']
     }
+},
+{
+    timestamps: true
 })
 module.exports = mongoose.model("user", userSchema)
